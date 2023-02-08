d9eccd05d82078f01d144325b613d4fabd4f5e10
Lukas N Wirz <lnwirz@chem.helsinki.fi>
2023-02-09 00:48:45 +0200

    readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# Space filling curve layouts for std::mdspan
+# Experimental 2D space filling curve layouts for std::mdspan
 
-## so far: 2d only, Hilbert, Morton
+So far there are 2 space filling curves implemented: Hilbert (2^8x2^8) and
+Morton (2^8x2^8 or 2^16x2^16).  For either of them the layout places squares in
+row major order; an image type making use of them sacrifices memory for speed
+if the requested image size is not a multiple of the block size.  Larger blocks
+may offer better cache line utilization (with quickly diminishing returns) at
+the expense of more wasted memory and more expensive index computations.
+
+
--- src/example.cpp
+++ src/example.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 
-#include "src/hilbert.h"
-#include "src/morton.h"
-
 #include "src/hilbert-layout.h"
 #include "src/morton-layout.h"
 
@@ -19,16 +16,16 @@ int main() {
   // stx::mdspan<int, stx::extents<uint64_t, 3, 3>, stx::layout_left> ml{d.data()};
   // stx::mdspan<int, stx::extents<uint64_t, 3, 3>, stx::layout_right> mr{d.data()};
 
-  for (int y = 0; y < 16; y++) {
-    for (int x = 0; x < 16; x++) {
+  for (int y = 0; y < 32; y++) {
+    for (int x = 0; x < 32; x++) {
       std::cout << mh[x, y] << " ";
     }
     std::cout << std::endl;
   }
 
   std::cout << std::endl;
-  for (int y = 0; y < 16; y++) {
-    for (int x = 0; x < 16; x++) {
+  for (int y = 0; y < 32; y++) {
+    for (int x = 0; x < 32; x++) {
       std::cout << mm[x, y] << " ";
     }
     std::cout << std::endl;
