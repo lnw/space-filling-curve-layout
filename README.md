631f4bf9d8ec84fec09746e4d0b07e361a4e1b73
Lukas N Wirz <lnwirz@chem.helsinki.fi>
2023-02-16 01:17:45 +0200

    benchmarking setup

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Experimental 2D space filling curve layouts for std::mdspan
 
-So far there are 2 space filling curves implemented: Hilbert (2^8x2^8) and
-Morton (2^8x2^8 or 2^16x2^16).  For either of them the layout places squares in
-row major order; an image type making use of them sacrifices memory for speed
-if the requested image size is not a multiple of the block size.  Larger blocks
-may offer better cache line utilization (with quickly diminishing returns) at
-the expense of more wasted memory and more expensive index computations.
+So far there are 2 space filling curves implemented: Hilbert (with blocks of
+256x256) and Morton (with blocks of 256x256 or 16kx16k).  For either of them
+the layout places squares in row major order; an image type making use of them
+sacrifices memory for speed if the requested image size is not a multiple of
+the block size.  Larger blocks may offer better cache line utilization (with
+quickly diminishing returns) at the expense of more wasted memory and more
+expensive index computations.
 
 
---
+++ benchmark_src/example.cpp
@@ -0,0 +1,166 @@
+#include "benchmark/benchmark.h"
+
+// #include <cstdio>
+#include <chrono>
+#include <random>
+// #include <iostream>
+
+#include "src/hilbert.h"
+#include "src/morton.h"
+
+std::random_device seed;
+std::mt19937 gen(seed());
+std::uniform_int_distribution<> distrib8(0, (1<<8)-1);
+std::uniform_int_distribution<> distrib16(0, (1<<15)-1);
+
+static void bm_8_0(benchmark::State& state) {
+  for (auto _ : state) {
+    uint16_t x = distrib8(gen);
+    uint16_t y = distrib8(gen);
+    benchmark::DoNotOptimize(x);
+    benchmark::DoNotOptimize(y);
+  }
+}
+static void bm_16_0(benchmark::State& state) {
+  for (auto _ : state) {
+    uint16_t x = distrib16(gen);
+    uint16_t y = distrib16(gen);
+    benchmark::DoNotOptimize(x);
+    benchmark::DoNotOptimize(y);
+  }
+}
+BENCHMARK(bm_8_0);
+BENCHMARK(bm_16_0);
+
+static void bm_hilbert8_1(benchmark::State& state) {
+  for (auto _ : state) {
+    uint16_t x = distrib8(gen);
+    uint16_t y = distrib8(gen);
+    auto res = hilbert8(x, y);
+    benchmark::DoNotOptimize(res);
+  }
+}
+static void bm_hilbert8_2(benchmark::State& state) {
+  for (auto _ : state) {
+    uint16_t x = distrib8(gen);
+    uint16_t y = distrib8(gen);
+    auto res1 = hilbert8(x, y);
+    auto res2 = hilbert8(y, x);
+    benchmark::DoNotOptimize(res1);
+    benchmark::DoNotOptimize(res2);
+  }
+}
+static void bm_hilbert8_4(benchmark::State& state) {
+  //state.PauseTiming();
+  //state.ResumeTiming();
+  for (auto _ : state) {
+    uint16_t x = distrib8(gen);
+    uint16_t y = distrib8(gen);
+    // state.ResumeTiming();
+    auto res1 = hilbert8(x, y);
+    auto res2 = hilbert8(y, x);
+    auto res3 = hilbert8(x+1, y+1);
+    auto res4 = hilbert8(y+1, x+1);
+    benchmark::DoNotOptimize(res1);
+    benchmark::DoNotOptimize(res2);
+    benchmark::DoNotOptimize(res3);
+    benchmark::DoNotOptimize(res4);
+  }
+}
+BENCHMARK(bm_hilbert8_1);
+BENCHMARK(bm_hilbert8_2);
+BENCHMARK(bm_hilbert8_4);
+
+
+static void bm_morton8_1(benchmark::State& state) {
+  // state.PauseTiming();
+  for (auto _ : state) {
+    uint16_t x = distrib8(gen);
+    uint16_t y = distrib8(gen);
+    // state.ResumeTiming();
+    auto res = morton8(x, y);
+    benchmark::DoNotOptimize(res);
+    // state.PauseTiming();
+  }
+}
+static void bm_morton8_2(benchmark::State& state) {
+  // state.PauseTiming();
+  for (auto _ : state) {
+    uint16_t x = distrib8(gen);
+    uint16_t y = distrib8(gen);
+    // state.ResumeTiming();
+    auto res1 = morton8(x, y);
+    auto res2 = morton8(y, x);
+    benchmark::DoNotOptimize(res1);
+    benchmark::DoNotOptimize(res2);
+    // state.PauseTiming();
+  }
+}
+static void bm_morton8_4(benchmark::State& state) {
+  // state.PauseTiming();
+  for (auto _ : state) {
+    uint16_t x = distrib8(gen);
+    uint16_t y = distrib8(gen);
+    // state.ResumeTiming();
+    auto res1 = morton8(x, y);
+    auto res2 = morton8(y, x);
+    auto res3 = morton8(x+1, y+1);
+    auto res4 = morton8(y+1, x+1);
+    benchmark::DoNotOptimize(res1);
+    benchmark::DoNotOptimize(res2);
+    benchmark::DoNotOptimize(res3);
+    benchmark::DoNotOptimize(res4);
+  }
+}
+BENCHMARK(bm_morton8_1);
+BENCHMARK(bm_morton8_2);
+BENCHMARK(bm_morton8_4);
+
+
+static void bm_morton16_1(benchmark::State& state) {
+  // state.PauseTiming();
+  for (auto _ : state) {
+    uint16_t x = distrib16(gen);
+    uint16_t y = distrib16(gen);
+    // state.ResumeTiming();
+    auto res = morton16(x, y);
+    benchmark::DoNotOptimize(res);
+    // state.PauseTiming();
+  }
+}
+static void bm_morton16_2(benchmark::State& state) {
+  // state.PauseTiming();
+  for (auto _ : state) {
+    uint16_t x = distrib16(gen);
+    uint16_t y = distrib16(gen);
+    // state.ResumeTiming();
+    auto res1 = morton16(x, y);
+    auto res2 = morton16(y, x);
+    benchmark::DoNotOptimize(res1);
+    benchmark::DoNotOptimize(res2);
+    // state.PauseTiming();
+  }
+}
+static void bm_morton16_4(benchmark::State& state) {
+  // state.PauseTiming();
+  for (auto _ : state) {
+    uint16_t x = distrib16(gen);
+    uint16_t y = distrib16(gen);
+    // state.ResumeTiming();
+    auto res1 = morton16(x, y);
+    auto res2 = morton16(y, x);
+    auto res3 = morton16(x+1, y+1);
+    auto res4 = morton16(y+1, x+1);
+    benchmark::DoNotOptimize(res1);
+    benchmark::DoNotOptimize(res2);
+    benchmark::DoNotOptimize(res3);
+    benchmark::DoNotOptimize(res4);
+    // state.PauseTiming();
+  }
+}
+BENCHMARK(bm_morton16_1);
+BENCHMARK(bm_morton16_2);
+BENCHMARK(bm_morton16_4);
+
+BENCHMARK_MAIN();
+
